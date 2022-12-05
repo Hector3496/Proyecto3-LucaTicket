736cd91914c6f0a0137d491e50bd4208295c873a
Raul <raul.lorca@gmail.com>
2022-12-05 11:13:04 +0100

    Actualizado

--- Eventos/Proyecto03-Eventos/src/main/java/com/example/eventos/repository/EventoRepository.java
+++ Eventos/Proyecto03-Eventos/src/main/java/com/example/eventos/repository/EventoRepository.java
@@ -1,9 +1,10 @@
 package com.example.eventos.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.eventos.model.Evento;
 
 /*Interface para realizar la llamada a metodos incluidos por defecto en la clase interna de JpaRepository*/
-public interface EventoRepository extends JpaRepository<Evento, Integer> {
+public interface EventoRepository extends MongoRepository<Evento, Integer> {
 }
