538530f5bf1fe2ad1b9aaa5f62464e6cd79ed24a
Hector3496 <hectorgonzalez.schez@gmail.com>
2022-12-06 09:16:20 +0100

    Open api

--- Eventos/Proyecto03-Eventos/src/main/java/com/example/eventos/config/OpenApiConfig.java
+++ Eventos/Proyecto03-Eventos/src/main/java/com/example/eventos/config/OpenApiConfig.java
@@ -18,14 +18,14 @@ public class OpenApiConfig {
     public OpenAPI VideojuegoOpenAPI() {
         return new OpenAPI()
                 .info(new Info().title("Videojuego API")
-                .description("Documentación del Videojuego API")
+                .description("Documentación del Eventos API")
                 .version("v1.0")
                 .contact(new Contact().name("Grupo 3").
                         url("https://grupo3.es").email("grupo@tres.es"))
                 .license(new License().name("LICENSE").url("http://springdoc.org")))
                 .externalDocs(new ExternalDocumentation()
                 .description("Proyecto de importar un CSV, Crear, Leer, Actualizar y Eliminar. Incluimos algunos listados de busquedas")
-                .url("http://localhost:8085/videojuegos"));
+                .url("http://localhost:8085/eventos"));
     }
 	
 
