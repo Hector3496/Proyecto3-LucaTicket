b955924d373fb3b48f0fcff8bc5b7814f5edaea6
Hector3496 <hectorgonzalez.schez@gmail.com>
2022-12-07 10:40:56 +0100

    Actualizado queries

--- Eventos/Proyecto03-Eventos/src/main/java/com/example/eventos/repository/EventoRepository.java
+++ Eventos/Proyecto03-Eventos/src/main/java/com/example/eventos/repository/EventoRepository.java
@@ -12,14 +12,16 @@ import com.example.eventos.model.Evento;
 /*Interface para realizar la llamada a metodos incluidos por defecto en la clase interna de JpaRepository*/
 public interface EventoRepository extends MongoRepository<Evento, Integer> {
 
-	@Query(value = "db.eventos.find({genre: ?1})")
+	//Query para el metodo mostrar los eventos por un genero
+	@Query(value = "db.eventoDB.find({genre: ?1})")
 	List<Evento> findByGenre(String genero);
-	/*
-     * Query para el metodo mostrar un evento por nombre en la base de datos
-     */
-	@Query("db.LucaTicket.find({nombre: ?1})")
+	
+	//Query para el metodo mostrar un evento por nombre en la base de datos
+	@Query("db.eventoDB.find({nombre: ?1})")
 	List<Evento> findByNombre(String nombre);
-	@Query("db.LucaTicket.find({salaCiudad: ?1})")
+	
+	//Query para el metodo mostrar los eventos de una ciudad
+	@Query("db.eventoDB.find({salaCiudad: ?1})")
 	List<Evento> findByCiudad(String ciudad);
 
 }
